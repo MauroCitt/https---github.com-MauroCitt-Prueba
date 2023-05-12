83d77c92a8f9fc1c217e075cfe79b35d16a11f41
MauroCitt <maci290@vidalibarraquer.net>
2023-05-12 09:55:44 +0200

    first commit

---
+++ src/main/java/com/mycompany/hola2/Hola2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.hola2;
+
+/**
+ *
+ * @author alumne
+ */
+public class Hola2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Estel");
+        System.out.println("Adiós Estel");
+    }
+}
